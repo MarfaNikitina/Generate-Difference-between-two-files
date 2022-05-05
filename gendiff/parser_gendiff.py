3775b120290fbcef8064cfcdbeb75ae23e28235f
Marfa Nikitina <marfik93@mail.ru>
2022-05-05 17:21:15 +0300

    2step

--- gendiff/parser_gendiff.py
+++ gendiff/parser_gendiff.py
@@ -4,10 +4,10 @@ import argparse
 
 def make_parser():
     parser = argparse.ArgumentParser(prog='gendiff',
-                                 usage='%(prog)s first_file second_file',
                                  description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
     config = vars(args)
     print(config)
