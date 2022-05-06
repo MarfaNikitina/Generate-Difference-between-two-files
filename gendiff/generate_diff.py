0bbb43848adf942dc00467494367c7ee09d1b9a7
Marfa Nikitina <marfik93@mail.ru>
2022-05-06 12:52:16 +0300

    06/05

---
+++ gendiff/generate_diff.py
@@ -0,0 +1,20 @@
+# !/usr/bin/env python3
+import json
+
+
+def generate_diff(file1, file2):
+    dict1 = json.load(open(f'gendiff.tests.fixtures.{file1}.json'))
+    dict2 = json.load(open(f'gendiff.tests.fixtures.{file2}.json'))
+    sorted_keys_set = sorted(set(dict1.keys()).union(set(dict2.keys())))
+    result_diff = ''
+    for key in sorted_keys_set:
+        if key in dict1.keys() and key in dict2.keys():
+            if dict1[key] == dict2[key]:
+                result_diff += f'{key}: {dict1[key]}'
+            else:
+                result_diff += f'{- key}: {dict1[key]}/n{+ key}: {dict2[key]}'
+        elif key in dict1.keys() and key not in dict2.keys():
+            result_diff += f'{- key}: {dict1[key]}'
+        elif key not in dict1.keys() and key in dict2.keys():
+            result_diff += f'{+ key}: {dict2[key]}'
+    return '{/n}'.join(result_diff)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,20 @@
 # !/usr/bin/env python3
-from gendiff.parser_gendiff import make_parser
+from gendiff.generate_diff import generate_diff
+
+
+import argparse
+
 
 def main():
-    make_parser()
+    parser = argparse.ArgumentParser(
+        prog='gendiff',
+        description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
+    args = parser.parse_args()
+    diff = generate_diff(args.first_file, args.second_file)
+    print(diff)
 
 
 if __name__ == '__main__':
