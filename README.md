669e35582071d3497cbe7b91e229957afd8d3d9a
Marfa Nikitina <marfik93@mail.ru>
2022-05-10 16:08:58 +0300

    10/05

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
  https://asciinema.org/a/WXXNKgX4YVlnhzqp6oueb7J0Z
 
 
-
+<a href="https://codeclimate.com/github/MarfaNikitina/python-project-lvl2/test_coverage"><img src="https://api.codeclimate.com/v1/badges/a5b198ab73245bf35f42/test_coverage" /></a>
 <a href="https://codeclimate.com/github/MarfaNikitina/python-project-lvl2/maintainability"><img src="https://api.codeclimate.com/v1/badges/a5b198ab73245bf35f42/maintainability" /></a>
 [![linter check](https://github.com/MarfaNikitina/python-project-lvl2/actions/workflows/hexlet-lint.yml/badge.svg)](https://github.com/MarfaNikitina/python-project-lvl2/actions/workflows/hexlet-lint.yml)
 [![Actions Status](https://github.com/MarfaNikitina/python-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/MarfaNikitina/python-project-lvl2/actions)
--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -33,7 +33,36 @@ def resf1f2_y():
     return open(f'tests/fixtures/f1f2_yml_diff.txt', "r").read()
 
 
+@pytest.fixture
+def f1path_j():
+    return 'file1path.json'
+
+
+@pytest.fixture
+def f2path_j():
+    return 'file2path.json'
+
+@pytest.fixture
+def f1path_y():
+    return 'file1path.yml'
+
+
+@pytest.fixture
+def f2path_y():
+    return 'file2path.yml'
+
+
+@pytest.fixture
+def resf1f2path():
+    return open(f'tests/fixtures/filepath12_diff.txt', "r").read()
+
+
 def test_generate_diff(f1_j, f2_j, resf1f2_j,
-                       f1_y, f2_y, resf1f2_y):
+                       f1_y, f2_y, resf1f2_y,
+                       f1path_j, f2path_j,
+                       f1path_y, f2path_y,
+                       resf1f2path):
     assert generate_diff(f1_j, f2_j) == resf1f2_j
     assert generate_diff(f1_y, f2_y) == resf1f2_y
+    assert generate_diff(f1path_j, f2path_j) == resf1f2path
+    assert generate_diff(f1path_y, f2path_y) == resf1f2path
