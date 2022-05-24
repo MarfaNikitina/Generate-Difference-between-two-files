51b3cc54dd330be78f84fc50a6109d70f5996bc8
Marfa Nikitina <marfik93@mail.ru>
2022-05-24 20:14:35 +0300

    24/05/01

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -27,14 +27,15 @@ def create_end_by_status(v):
         return "removed"
     elif v['STATUS'] == 'CHANGED':
         return f"updated. From {format_value(v['VALUE1'])} " \
-                     f"to {format_value(v['VALUE2'])}"
+               f"to {format_value(v['VALUE2'])}"
 
 
 def make_plain_format(diff_dict):
     result_list = []
     for k, v in diff_dict.items():
         if v['STATUS'] in ['CHANGED', 'ADDED', 'DELETED']:
-            result_list.append(f'Property \'{k}\' was {create_end_by_status(v)}')
+            result_list.append(f'Property \'{k}\''
+                               f' was {create_end_by_status(v)}')
         elif v['STATUS'] == 'UNCHANGED':
             result_list = result_list
         elif v['STATUS'] == 'HASCHILD':
@@ -49,7 +50,8 @@ def plain_for_child(v, k):
     result_list = []
     for key, val in child.items():
         if val['STATUS'] in ['CHANGED', 'ADDED', 'DELETED']:
-            result_list.append(f"Property '{parent}.{key}' was {create_end_by_status(val)}")
+            result_list.append(f"Property '{parent}.{key}'"
+                               f" was {create_end_by_status(val)}")
         elif val['STATUS'] == 'UNCHANGED':
             result_list = result_list
         elif val['STATUS'] == 'HASCHILD':
