5ecd1b2c7d606de2aca3de9de3cc6f9b0c2ab0ae
Marfa Nikitina <marfik93@mail.ru>
2022-05-25 20:07:09 +0300

    25/05/06

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -8,9 +8,9 @@ from gendiff.formatters.plain import make_plain_format
 
 def load_file_by_format(file):
     if file.endswith(".json"):
-        loaded_file = json.load(open(f'tests/fixtures/{file}'))
+        loaded_file = json.load(open(f'{file}'))
     elif file.endswith(".yml") or file.endswith(".yaml"):
-        loaded_file = yaml.safe_load(open(f'tests/fixtures/{file}'))
+        loaded_file = yaml.safe_load(open(f'{file}'))
     return loaded_file
 
 
