0aa08d17a06fb4d7b2b3cbe1fd9d4480d708ffd8
Marfa Nikitina <marfik93@mail.ru>
2022-06-06 11:42:46 +0300

    06/06/01

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -8,6 +8,19 @@ TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
 FIXTURES_PATH = f"{TESTS_DIR}/fixtures"
 
 
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [((f"{FIXTURES_PATH}/file1.json",
+      f"{FIXTURES_PATH}/file2.json"),
+      open(f"{FIXTURES_PATH}/file1file2_json_diff.txt", "r").read()),
+    ((f"{FIXTURES_PATH}/file1.yml",
+      f"{FIXTURES_PATH}/file2.yml"),
+      open(f"{FIXTURES_PATH}/file1file2_yml_diff.txt", "r").read()),
+     ])
+def test_gendiff(test_input, expected):
+    assert generate_diff(test_input[0], test_input[1]) == expected
+
+
 @pytest.fixture
 def f1_j():
     return f"{FIXTURES_PATH}/file1.json"
