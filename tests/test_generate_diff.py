3164addc344f103633c0693bfa9c6dcb2fa73109
Marfa Nikitina <marfik93@mail.ru>
2022-06-06 14:45:15 +0300

    06/06/03

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -16,102 +16,50 @@ FIXTURES_PATH = f"{TESTS_DIR}/fixtures"
         ((f"{FIXTURES_PATH}/file1.yml",
             f"{FIXTURES_PATH}/file2.yml"),
             open(f"{FIXTURES_PATH}/f1f2_yml_diff.txt", "r").read()),
+        ((f"{FIXTURES_PATH}/file1tree.json",
+            f"{FIXTURES_PATH}/file2tree.json"),
+            open(f"{FIXTURES_PATH}/f12tree_diff.txt", "r").read()),
+        ((f"{FIXTURES_PATH}/file1tree.yml",
+            f"{FIXTURES_PATH}/file2tree.yml"),
+            open(f"{FIXTURES_PATH}/f12tree_diff.txt", "r").read())
      ])
 def test_gendiff(test_input, expected):
     assert generate_diff(test_input[0], test_input[1]) == expected
 
 
-@pytest.fixture
-def f1_j():
-    return f"{FIXTURES_PATH}/file1.json"
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [((f"{FIXTURES_PATH}/file1.json",
+      f"{FIXTURES_PATH}/file2.json"),
+      open(f"{FIXTURES_PATH}/f1f2plain_diff.txt", "r").read()),
+        ((f"{FIXTURES_PATH}/file1.yml",
+            f"{FIXTURES_PATH}/file2.yml"),
+            open(f"{FIXTURES_PATH}/f1f2plain_diff.txt", "r").read()),
+        ((f"{FIXTURES_PATH}/file1tree.json",
+            f"{FIXTURES_PATH}/file2tree.json"),
+            open(f"{FIXTURES_PATH}/plain_diff.txt", "r").read()),
+        ((f"{FIXTURES_PATH}/file1tree.yml",
+            f"{FIXTURES_PATH}/file2tree.yml"),
+            open(f"{FIXTURES_PATH}/plain_diff.txt", "r").read())
+     ])
+def test_plain_gendiff(test_input, expected):
+    assert generate_diff(test_input[0], test_input[1], format_name='plain') == expected
 
 
-@pytest.fixture
-def f2_j():
-    return f"{FIXTURES_PATH}/file2.json"
-
-
-@pytest.fixture
-def resf1f2_j():
-    return open(f"{FIXTURES_PATH}/file1file2_json_diff.txt", "r").read()
-
-
-@pytest.fixture
-def f1_y():
-    return f"{FIXTURES_PATH}/file1.yml"
-
-
-@pytest.fixture
-def f2_y():
-    return f"{FIXTURES_PATH}/file2.yml"
-
-
-@pytest.fixture
-def resf1f2_y():
-    return open(f"{FIXTURES_PATH}/f1f2_yml_diff.txt", "r").read()
-
-
-@pytest.fixture
-def f1tree_j():
-    return f"{FIXTURES_PATH}/file1tree.json"
-
-
-@pytest.fixture
-def f2tree_j():
-    return f"{FIXTURES_PATH}/file2tree.json"
-
-
-@pytest.fixture
-def f1tree_y():
-    return f"{FIXTURES_PATH}/file1tree.yml"
-
-
-@pytest.fixture
-def f2tree_y():
-    return f"{FIXTURES_PATH}/file2tree.yml"
-
-
-@pytest.fixture
-def resf1f2tree():
-    return open(f"{FIXTURES_PATH}/f12tree_diff.txt", "r").read()
-
-
-@pytest.fixture
-def plain_diff():
-    return open(f"{FIXTURES_PATH}/plain_diff.txt", "r").read()
-
-
-@pytest.fixture
-def json_tree_diff():
-    return open(f"{FIXTURES_PATH}/jsondiff.txt", "r").read()
-
-
-@pytest.fixture
-def f1f2_json_diff():
-    return open(f"{FIXTURES_PATH}/f1f2json_diff.txt", "r").read()
-
-
-@pytest.fixture
-def f1f2_plaindiff():
-    return open(f"{FIXTURES_PATH}/f1f2plain_diff.txt", "r").read()
-
-
-def test_generate_diff(f1_j, f2_j, resf1f2_j,
-                       f1_y, f2_y, resf1f2_y,
-                       f1tree_j, f2tree_j,
-                       f1tree_y, f2tree_y,
-                       resf1f2tree, plain_diff,
-                       json_tree_diff, f1f2_json_diff,
-                       f1f2_plaindiff):
-    assert generate_diff(f1_j, f2_j) == resf1f2_j
-    assert generate_diff(f1_y, f2_y) == resf1f2_y
-    assert generate_diff(f1tree_j, f2tree_j) == resf1f2tree
-    assert generate_diff(f1tree_y, f2tree_y) == resf1f2tree
-    assert generate_diff(f1tree_j, f2tree_j, format_name='plain') == plain_diff
-    assert generate_diff(f1tree_y, f2tree_y, format_name='plain') == plain_diff
-    assert generate_diff(f1_j, f2_j, format_name='plain') == f1f2_plaindiff
-    assert generate_diff(f1_y, f2_y, format_name='plain') == f1f2_plaindiff
-    assert generate_diff(f1tree_y, f2tree_y, format_name='json') == json_tree_diff
-    assert generate_diff(f1tree_j, f2tree_j, format_name='json') == json_tree_diff
-    assert generate_diff(f1_j, f2_j, format_name='json') == f1f2_json_diff
-    assert generate_diff(f1_y, f2_y, format_name='json') == f1f2_json_diff
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [((f"{FIXTURES_PATH}/file1.json",
+      f"{FIXTURES_PATH}/file2.json"),
+      open(f"{FIXTURES_PATH}/f1f2json_diff.txt", "r").read()),
+        ((f"{FIXTURES_PATH}/file1.yml",
+            f"{FIXTURES_PATH}/file2.yml"),
+            open(f"{FIXTURES_PATH}/f1f2json_diff.txt", "r").read()),
+        ((f"{FIXTURES_PATH}/file1tree.json",
+            f"{FIXTURES_PATH}/file2tree.json"),
+            open(f"{FIXTURES_PATH}/jsondiff.txt", "r").read()),
+     ((f"{FIXTURES_PATH}/file1tree.yml",
+       f"{FIXTURES_PATH}/file2tree.yml"),
+      open(f"{FIXTURES_PATH}/jsondiff.txt", "r").read())
+     ])
+def test_json_gendiff(test_input, expected):
+    assert generate_diff(test_input[0], test_input[1], format_name='json') == expected
