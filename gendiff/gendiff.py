aaa083afb8e3af5ba94db1348c5e04d5cf9126ef
Marfa Nikitina <marfik93@mail.ru>
2022-06-06 15:14:13 +0300

    06/06/03

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,5 +1,6 @@
 from gendiff.formatter import make_format
-from gendiff.parser import load_file_by_format
+from gendiff.parser import parse
+import os
 
 
 def calculate_diff(dict1, dict2):
@@ -40,7 +41,19 @@ def make_status_value_for_key(dict1, dict2, key):
 
 
 def generate_diff(file1, file2, format_name='stylish'):
-    dict1 = load_file_by_format(file1)
-    dict2 = load_file_by_format(file2)
+    with open(file1, "r") as data:
+        dict1 = parse(data, define_file_type(file1))
+    with open(file2, "r") as data:
+        dict2 = parse(data, define_file_type(file2))
     diff = calculate_diff(dict1, dict2)
     return make_format(diff, format_name)
+
+    # dict1 = load_file_by_format(file1)
+    # dict2 = load_file_by_format(file2)
+    # diff = calculate_diff(dict1, dict2)
+    # return make_format(diff, format_name)
+
+
+def define_file_type(filepath):
+    file_name, file_type = os.path.splitext(filepath)
+    return file_type
--- gendiff/parser.py
+++ gendiff/parser.py
@@ -2,9 +2,17 @@ import json
 import yaml
 
 
-def load_file_by_format(file):
-    if file.endswith(".json"):
-        loaded_file = json.load(open(f'{file}'))
-    elif file.endswith(".yml") or file.endswith(".yaml"):
-        loaded_file = yaml.safe_load(open(f'{file}'))
-    return loaded_file
+def parse_data(data, format):
+    if format == ".json":
+        return json.load(data)
+    elif format == '.yaml' or format == '.yml':
+        return yaml.safe_load(data)
+
+
+
+# def load_file_by_format(file):
+    # if file.endswith(".json"):
+        # loaded_file = json.load(open(f'{file}'))
+    # elif file.endswith(".yml") or file.endswith(".yaml"):
+        # loaded_file = yaml.safe_load(open(f'{file}'))
+    # return loaded_file
