869e985761189c49f6f020f3004d0658f5a88abb
Marfa Nikitina <marfik93@mail.ru>
2022-06-08 15:13:30 +0300

    07/06/01

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -9,42 +9,30 @@ FIXTURES_PATH = f"{TESTS_DIR}/fixtures"
 
 
 @pytest.mark.parametrize(
-    "file1,file2,filepath",
-    [("file1.json", "file2.json", "file1file2_json_diff.txt"),
-        ("file1.yml", "file2.yml", "f1f2_yml_diff.txt"),
-     ("file1tree.json", "file2tree.json", "f12tree_diff.txt"),
-        ("file1tree.yml", "file2tree.yml", "f12tree_diff.txt")
-     ])
+    "file1,file2,{filepath}",
+    [("file1.json", "file2.json",
+      {'STYLISH': "file1file2_json_diff.txt",
+       'PLAIN': "f1f2plain_diff.txt",
+       'JSON': "f1f2json_diff.txt"}),
+        ("file1.yml", "file2.yml",
+         {'STYLISH': "file1file2_yml_diff.txt",
+          'PLAIN': "f1f2plain_diff.txt",
+          'JSON': "f1f2json_diff.txt"}),
+     ("file1tree.json", "file2tree.json",
+      {'STYLISH': "f12tree_diff.txt",
+       'PLAIN': "plain_diff.txt",
+       'JSON': "jsondiff.txt"}),
+        ("file1tree.yml", "file2tree.yml",
+         {'STYLISH': "f12tree_diff.txt",
+          'PLAIN': "plain_diff.txt",
+          'JSON': "jsondiff.txt"}
+         )])
 def test_gendiff(file1, file2, filepath):
     f1 = f"{FIXTURES_PATH}/{file1}"
     f2 = f"{FIXTURES_PATH}/{file2}"
-    expected = open(f"{FIXTURES_PATH}/{filepath}", "r").read()
+    expected = open(f"{FIXTURES_PATH}/{filepath['STYLISH']}", "r").read()
+    expected_plain = open(f"{FIXTURES_PATH}/{filepath['PLAIN']}", "r").read()
+    expected_json = open(f"{FIXTURES_PATH}/{filepath['JSON']}", "r").read()
     assert generate_diff(f1, f2) == expected
-
-
-@pytest.mark.parametrize(
-    "file1,file2,filepath",
-    [("file1.json", "file2.json", "f1f2plain_diff.txt"),
-        ("file1.yml", "file2.yml", "f1f2plain_diff.txt"),
-     ("file1tree.json", "file2tree.json", "plain_diff.txt"),
-        ("file1tree.yml", "file2tree.yml", "plain_diff.txt")
-     ])
-def test_plain_gendiff(file1, file2, filepath):
-    f1 = f"{FIXTURES_PATH}/{file1}"
-    f2 = f"{FIXTURES_PATH}/{file2}"
-    expected = open(f"{FIXTURES_PATH}/{filepath}", "r").read()
-    assert generate_diff(f1, f2, format_type='plain') == expected
-
-
-@pytest.mark.parametrize(
-    "file1,file2,filepath",
-    [("file1.json", "file2.json", "f1f2json_diff.txt"),
-        ("file1.yml", "file2.yml", "f1f2json_diff.txt"),
-     ("file1tree.json", "file2tree.json", "jsondiff.txt"),
-        ("file1tree.yml", "file2tree.yml", "jsondiff.txt")
-     ])
-def test_json_gendiff(file1, file2, filepath):
-    f1 = f"{FIXTURES_PATH}/{file1}"
-    f2 = f"{FIXTURES_PATH}/{file2}"
-    expected = open(f"{FIXTURES_PATH}/{filepath}", "r").read()
-    assert generate_diff(f1, f2, format_type='json') == expected
+    assert generate_diff(f1, f2, format_type='plain') == expected_plain
+    assert generate_diff(f1, f2, format_type='json') == expected_json
