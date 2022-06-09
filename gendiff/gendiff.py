007e38a6f62134391a5ee1855f19e8b3e96bc958
Marfa Nikitina <marfik93@mail.ru>
2022-06-09 20:48:37 +0300

    lala

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -5,9 +5,9 @@ import os
 
 
 def generate_diff(file1, file2, format_type='stylish'):
-    with open(file1, "r") as data:
+    with open(f'tests/fixtures/{file1}', "r") as data:
         dict1 = parse(data, os.path.splitext(file1)[1])
-    with open(file2, "r") as data:
+    with open(f'tests/fixtures/{file2}', "r") as data:
         dict2 = parse(data, os.path.splitext(file2)[1])
     diff = calculate_diff(dict1, dict2)
     return format(diff, format_type)
