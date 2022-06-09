4e8910aefa6f7fe2c7610e6223e55c079a82bdcf
Marfa Nikitina <marfik93@mail.ru>
2022-06-09 21:13:48 +0300

    lala

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,13 +3,13 @@ from gendiff import generate_diff
 from gendiff.cli import parse
 
 
-FIXTURES_FILES_PATH = 'tests/fixtures'
+# FIXTURES_FILES_PATH = 'tests/fixtures'
 
 
 def main():
     args = parse()
-    diff = generate_diff(f'{FIXTURES_FILES_PATH}/{args.first_file}',
-                         f'{FIXTURES_FILES_PATH}/{args.second_file}',
+    diff = generate_diff(args.first_file,
+                         args.second_file,
                          args.format)
     print(diff)
 
